29db0b759006b219649348f442b1e3da7d8f0f66
Thomas Risberg <trisberg@pivotal.io>
2015-04-27 15:59:15 -0400

    Updating imports

--- hello-java/src/main/java/com/springdeveloper/demo/Application.java
+++ hello-java/src/main/java/com/springdeveloper/demo/Application.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.hadoop.fs.FsShell;
 
